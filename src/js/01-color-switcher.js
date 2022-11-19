fdc9afb615f06431e58aac5173642575c618aa06
AlyonaKorniienko <vkusniashka.pp.ua@gmail.com>
2022-11-19 11:14:41 +0200

    btn fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,43 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// let getEl = selector => document.querySelector(selector);
+
+// // onStartClick;
+// getEl('[data-start]').addEventListener('click', evt => {
+//   timerId = setInterval(() => {
+//     getEl('body').style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+//   getEl('[data-start]').disabled = true;
+// });
+
+// // onStopClick;
+// getEl('[data-stop]').addEventListener('click', e => {
+//   clearInterval(timerId);
+//   getEl('[data-start]').disabled = false;
+// });
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-let getEl = selector => document.querySelector(selector);
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+// console.log(startBtn);
+const body = document.querySelector('body');
+// console.log(body);
 
 // onStartClick;
-getEl('[data-start]').addEventListener('click', evt => {
+startBtn.addEventListener('click', evt => {
   timerId = setInterval(() => {
-    getEl('body').style.backgroundColor = getRandomHexColor();
+    body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-  getEl('[data-start]').disabled = true;
+  startBtn.disabled = true;
 });
 
 // onStopClick;
-getEl('[data-stop]').addEventListener('click', e => {
+stopBtn.addEventListener('click', e => {
   clearInterval(timerId);
-  getEl('[data-start]').disabled = false;
+  startBtn.disabled = false;
 });
