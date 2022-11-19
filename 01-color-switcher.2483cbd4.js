fa42cc6768e6a5223f75dcf9b0d1c5b122a3a442
AlyonaKorniienko <105073659+AlyonaKorniienko@users.noreply.github.com>
2022-11-19 09:36:03 +0000

    Deploying to gh-pages from @ AlyonaKorniienko/goit-js-hw-09@02250a71666741a19992c5dd688a0c0f05387856 🚀

--- 01-color-switcher.2483cbd4.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),o=document.querySelector("body");e.addEventListener("click",(function(t){timerId=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0,console.log(e)})),t.addEventListener("click",(t=>{clearInterval(timerId),e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.2483cbd4.js.map
--- 01-color-switcher.f0c51c9f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(function(e){timerId=setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,console.log(t)})),e.addEventListener("click",(function(e){clearInterval(timerId),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.f0c51c9f.js.map
